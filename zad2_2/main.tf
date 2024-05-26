f6b82bba1e0a3eec3d8b7a42d076a17a769564c6
CyprianVolue <cyprian.pelka@volue.com>
2024-05-26 20:50:31 +0200

    dodane zadanie 2 z drugiej listy

---
+++ zad2_2/main.tf
@@ -0,0 +1,44 @@
+# Provider Configuration (Azure)
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+
+# Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = var.resource_group_location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+}
+
+# Storage Container
+resource "azurerm_storage_container" "example" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+
+resource "azurerm_storage_blob" "example" {
+  name                   = var.storage_blob_name
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = var.storage_blob_source
+}
